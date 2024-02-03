86a452d8be100f0d95d808a04b4deeb2d49d6f1a
Sidharth Rao <sidharthmrao@gmail.com>
2024-02-03 12:56:31 -0500

    Refactored.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from algorithm import rrt_sid, rrt_star
+from global_sim.algorithm import rrt_sid, rrt_star
 from type_hints.types import Node
 
 # Algorithm
@@ -7,16 +7,16 @@ algo_options = {
     "rrt_star": rrt_star
 }
 
-algo: str = "rrt_sid"
+algo: str = "rrt_star"
 
-iters: int = 7000
+iters: int = 1000
 start_node: Node = (5, 5)
 end_node: Node = (340, 340)
 initial_generate_dist: int = 50  # Sometimes, increasing leads to reaching farther areas
 
 
 # Render
-map_bw_reverse: bool = True  # Reverse black and white in obstacle readings
+map_bw_reverse: bool = False  # Reverse black and white in obstacle readings
 display_end: bool = False  # Display final image when program finished
 
 # Debug
@@ -25,9 +25,9 @@ debug_time: float = 0  # Time to wait after every frame displayed in seconds
 debug_iters: int = 50  # Display frame ever 'debug_iters' iters
 
 # Map Path
-map_path = 'maps/img_5.png'
+map_path = 'maps/img_3.png'
 
 # Output image directory
-out_path = './out/test.png'
+out_path = 'global_sim/out/test.png'
 
 
